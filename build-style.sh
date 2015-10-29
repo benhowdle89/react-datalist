0b0fd745d72fcb6802da29d0dae794b5e1c7d534
Ben Howdle <benhowdle89@gmail.com>
2015-10-29 14:56:47 +0000

    stylus

--- build-style.sh
+++ build-style.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-stylus src/styles/react-datalist.styl -p | sed ':a;N;$!ba;s/\n/ /g' | xargs -I css replace '_interopRequire\(require\("./styles/react-datalist.styl"\)\);' '"css"' lib/ReactDataList.js
+stylus src/styles/react-datalist.styl -p | sed ':a;N;$!ba;s/\n/ /g'
+# | xargs -I css replace '_interopRequire\(require\("./styles/react-datalist.styl"\)\);' '"css"' lib/ReactDataList.js
